00bf3c1cb990ceb4e30b2c5df1a9c3152d214d27
David Felix vieira <davidfelixvieira560@gmail.com>
2026-08-14 20:51:39 -0300

    pagina de criar sala criada

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
-from database import inicializar_banco
+from flask import Flask, render_template, request, redirect
+from database import inicializar_banco, conectar
+import secrets
+import string
 
 app = Flask(__name__)
 
@@ -11,9 +13,33 @@ def inicio():
     return render_template("index.html")
 
 
-@app.route("/criar-sala")
+@app.route("/criar-sala", methods=["GET", "POST"])
 def criar_sala():
-    return "Página para criar sala"
+
+    if request.method == "POST":
+        nome = request.form["nome"]
+
+        caracteres = string.ascii_uppercase + string.digits # Gera um conjunto de caracteres que inclui letras maiúsculas e números
+        codigo = "".join(secrets.choice(caracteres) for _ in range(5)) # Gera um código aleatório de 5 caracteres (letras maiúsculas e números)
+
+        token_mestre = secrets.token_hex(32) # Gera um token aleatório de 32 caracteres
+
+        conexao = conectar()
+
+        conexao.execute(
+            """
+            INSERT INTO salas (codigo, nome, token_mestre)
+            VALUES (?, ?, ?)
+            """,
+            (codigo, nome, token_mestre)
+        )
+
+        conexao.commit()
+        conexao.close()
+
+        return f"Sala criada! Código: {codigo}"
+
+    return render_template("criar_sala.html")
 
 
 @app.route("/entrar-sala")
